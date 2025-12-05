a246e35c2372cc2cf089d42432e0cc1718ca7222
sandra-s13 <sandrasurendran138@gmail.com>
2025-12-05 22:58:58 +0530

    american development

--- src/test/java/Selenium_Test.java
+++ src/test/java/Selenium_Test.java
@@ -1,4 +1,14 @@
 
 public class Selenium_Test {
+	
+
+public void SUbjects() {
+	System.out.println("first output");
+	System.out.println("second output");
+}
+public void Students() {
+	System.out.println("first output");
+	System.out.println("second output");
+}
 
 }
